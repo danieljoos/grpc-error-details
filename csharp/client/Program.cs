2899286d446f1fc6d3fbdcdea9d7a03d7059efeb
Daniel Joos <daniel@joosweb.de>
2018-04-04 21:09:32 +0200

    Added csharp client example project
    This adds the GRPC client example for csharp receiving an error with a custom protobuf object in the error-details section.

---
+++ csharp/client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Grpc.Core;
+
+namespace Example
+{
+    public static class RpcExceptionExtensions
+    {
+        public static Google.Rpc.Status ExtractErrorDetails(this RpcException exception)
+        {
+            return exception.Trailers
+                .Where(x => x.Key.Equals("grpc-status-details-bin"))
+                .Select(x => Google.Rpc.Status.Parser.ParseFrom(x.ValueBytes))
+                .DefaultIfEmpty(null)
+                .First();
+        }
+
+        public static T ExtractErrorDetails<T>(this RpcException exception)
+            where T : Google.Protobuf.IMessage, new()
+        {
+            T result = default(T);
+            exception
+                .ExtractErrorDetails()?.Details
+                .FirstOrDefault()
+                .TryUnpack<T>(out result);
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:50001", ChannelCredentials.Insecure);
+            var client = new ExampleHello.ExampleHelloClient(channel);
+            try
+            {
+                var response = client.SaySomething(new SaySomethingRequest { Phrase = "Foo" });
+                Console.WriteLine($"Server responded: { response.Echo }");
+            }
+            catch (RpcException ex)
+            {
+                var details = ex.ExtractErrorDetails<ErrorDetails>();
+                if (details != null)
+                {
+                    Console.WriteLine($"error-details: { details.Why }");
+                }
+            }
+            catch (Exception)
+            {
+            }
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
