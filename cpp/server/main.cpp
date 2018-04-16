5a39a2c73415b10eda267b41cd37aef49b367b60
Daniel Joos <daniel@joosweb.de>
2018-04-16 22:20:45 +0200

    Added clang-format config for C++ server example

--- cpp/server/main.cpp
+++ cpp/server/main.cpp
@@ -10,34 +10,35 @@
 #include "status.pb.h"
 
 class ExampleHelloImpl final : public example::ExampleHello::Service {
-  grpc::Status SaySomething(
-      grpc::ServerContext* context, const example::SaySomethingRequest* request,
-      example::SaySomethingResponse* response) final override {
-    // Create a protobuf message object that represents our custom information
-    example::ErrorDetails details;
-    details.set_why("some detailed error information");
+    grpc::Status SaySomething(grpc::ServerContext* context,
+                              const example::SaySomethingRequest* request,
+                              example::SaySomethingResponse* response) final override {
 
-    // Create a google::rpc::Status protobuf message object that will be sent in
-    // the grpc trailers. The custom error-details object (see above) will be
-    // packed into an `Any` field.
-    google::rpc::Status status;
-    status.set_code(grpc::StatusCode::UNKNOWN);
-    status.set_message("normal error message");
-    status.add_details()->PackFrom(details);
+        // Create a protobuf message object that represents our custom information
+        example::ErrorDetails details;
+        details.set_why("some detailed error information");
 
-    // Call the `SetErrorDetails` support function of grpc.
-    grpc::Status result;
-    grpc::SetErrorDetails(status, &result);
-    return result;
-  }
+        // Create a google::rpc::Status protobuf message object that will be sent in
+        // the grpc trailers. The custom error-details object (see above) will be
+        // packed into an `Any` field.
+        google::rpc::Status status;
+        status.set_code(grpc::StatusCode::UNKNOWN);
+        status.set_message("normal error message");
+        status.add_details()->PackFrom(details);
+
+        // Call the `SetErrorDetails` support function of grpc.
+        grpc::Status result;
+        grpc::SetErrorDetails(status, &result);
+        return result;
+    }
 };
 
 int main(int argc, char** argv) {
-  ExampleHelloImpl service;
-  grpc::ServerBuilder builder;
-  builder.AddListeningPort("127.0.0.1:50001",
-                           grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  builder.BuildAndStart()->Wait();
-  return 0;
+    ExampleHelloImpl service;
+    grpc::ServerBuilder()
+        .AddListeningPort("127.0.0.1:50001", grpc::InsecureServerCredentials())
+        .RegisterService(&service)
+        .BuildAndStart()
+        ->Wait();
+    return 0;
 }
